7dca4d82faa78a7beca21ce1c860bee5fc7459f4
Richasy <Thansy@foxmail.com>
2024-05-24 22:05:50 +0800

    Update

--- dotnet/src/Connectors/Connectors.SparkDesk/SparkDeskPromptExecutionSettings.cs
+++ dotnet/src/Connectors/Connectors.SparkDesk/SparkDeskPromptExecutionSettings.cs
@@ -1,7 +1,6 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.Text.Json;
 using System;
 using System.Text.Json.Serialization;
